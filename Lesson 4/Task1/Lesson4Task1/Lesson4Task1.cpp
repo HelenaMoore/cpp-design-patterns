3e688f6c9809e420181427cbff6b29a9529b42e6
Elena An <moore.helena@yandex.ru>
2023-05-21 10:48:38 +0300

    add lesson 4 homeworks

---
+++ Lesson 4/Task1/Lesson4Task1/Lesson4Task1.cpp
@@ -0,0 +1,78 @@
+﻿#include <iostream>
+#include <fstream>
+#include <string>
+
+class Printable
+{
+public:
+    virtual ~Printable() = default;
+
+    virtual std::string print() const = 0;
+};
+
+class DataHTML : public Printable
+{
+public:
+    DataHTML(std::string data)
+        : data_(std::move(data)) {}
+
+    std::string print() const override
+    {
+        return "<html>" + data_ + "<html/>";
+    }
+private:
+    std::string data_;
+};
+
+class DataText : public Printable
+{
+public:
+    DataText(std::string data)
+        : data_(std::move(data)) {}
+
+    std::string print() const override
+    {
+        return data_;
+    }
+private:
+    std::string data_;
+};
+
+class DataJSON: public Printable
+{
+public:
+    DataJSON(std::string data)
+        : data_(std::move(data)) {}
+
+    std::string print() const override
+    {
+        return "{ \"data\": \"" + data_ + "\"}";
+    }
+private:
+    std::string data_;
+};
+
+void saveTo(std::ofstream& file, const Printable& printable)
+{
+    file << printable.print();     
+}
+
+int main(int argc, char** argv)
+{
+    DataHTML data_html("sample html data");
+    DataText data_text("sample text data");
+    DataJSON data_json("sample json data");
+
+    std::ofstream write_file("out.txt", std::ios::app);
+    if (write_file.is_open())
+    {
+        saveTo(write_file, data_html);
+        write_file << std::endl;
+        saveTo(write_file, data_text);
+        write_file << std::endl;
+        saveTo(write_file, data_json);
+    }
+    write_file.close();
+
+    return 0;
+}
